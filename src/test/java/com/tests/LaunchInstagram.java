96f795d31b086b50326039cee38e9a1790de94c7
Asha <ashanayakaa@gmail.com>
2025-09-06 13:14:06 +0530

    Made Webdriver as global

--- src/test/java/com/tests/LaunchInstagram.java
+++ src/test/java/com/tests/LaunchInstagram.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchInstagram {
-
+	private static WebDriver driver;
+	
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.instagram.com/");
 		
