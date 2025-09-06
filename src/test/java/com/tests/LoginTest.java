5a57ddef58ab83db51a892fdd590e44026ff6bfc
bharathijs4 <naveenkrishna.tumkur@gmail.com>
2025-09-06 12:36:59 -0700

    login page added

--- src/test/java/com/tests/LoginTest.java
+++ src/test/java/com/tests/LoginTest.java
@@ -1,7 +1,14 @@
 package com.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class LoginTest {
-
+public class LoginTest 
+{
+ public static void main(String[]args)
+ {
+	 WebDriver driver=new ChromeDriver();
+	 driver.manage().window().maximize();
+	 driver.get("https://www.amazon.com");
+ }
 }
