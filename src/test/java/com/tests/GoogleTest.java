2cd1bdbde6bc9de5892d94dec3d5d7485a43600a
veereanna <btveeresh24@gmail.com>
2025-09-06 12:02:01 +0530

    Google Test added

---
+++ src/test/java/com/tests/GoogleTest.java
@@ -0,0 +1,24 @@
+package com.tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.com/");
+		String text = driver.getTitle();
+       System.out.println(text +":: Page navigation done to google");
+		
+       
+       
+	}
+
+}
