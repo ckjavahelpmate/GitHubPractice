df95e1c5b3c3bb50cdd11ea7a81830547627d057
veereanna <btveeresh24@gmail.com>
2025-09-06 12:30:53 +0530

    Facebook Page Added

---
+++ src/test/java/com/tests/FacebookTest.java
@@ -0,0 +1,21 @@
+package com.tests;
+
+import java.time.Duration;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.facebook.com/");
+		String facebookTitle = driver.getTitle();
+		System.out.println(facebookTitle +"::Facebook media launch");
+
+	}
+
+}
